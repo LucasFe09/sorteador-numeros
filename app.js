bb28807195f0f8ee3d0b8a7733f5411b4511c2d9
LucasFe97 <lucasferreirasd82@gmail.com>
2024-07-08 16:50:54 -0300

    limpando código

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function sortear() {
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`
     alterarStatusBotao();
 
-}
+
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
